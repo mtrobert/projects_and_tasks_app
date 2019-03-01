cdc14dd23f1136aec9b802553eb28491067c1d33
Robert Marczak <robert.m.1305@gmail.com>
2019-03-01 22:40:07 +0000

    Used Schema class and changed default string length to 191 in the boot method.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\{Facades\Schema, ServiceProvider};
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -13,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+      //
     }
 
     /**
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+
+      Schema::defaultStringLength(191);
+              //
     }
 }
