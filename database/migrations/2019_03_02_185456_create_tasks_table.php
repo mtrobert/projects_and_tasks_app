1f699989691df4e96c0451eec4a19995a2bde40a
Robert Marczak <robert.m.1305@gmail.com>
2019-03-02 18:56:53 +0000

    Recreated migration file for creating new tasks table.

--- database/migrations/2019_03_02_185456_create_tasks_table.php
+++ database/migrations/2019_03_02_185456_create_tasks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTableTasks extends Migration
+class CreateTasksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateTableTasks extends Migration
      */
     public function up()
     {
-        Schema::create('table_tasks', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateTableTasks extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('table_tasks');
+        Schema::dropIfExists('tasks');
     }
 }
