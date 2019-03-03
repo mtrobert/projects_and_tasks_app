2c0991bd5e15ff563993b03838d5bd3f932f0f7b
Robert Marczak <robert.m.1305@gmail.com>
2019-03-03 19:53:57 +0000

    Added container for title and container for viewing all project's titles and owners.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,23 @@
 @extends('layout')
+
+@section('content')
+  <div class="container box hero is-dark has-text-centered">
+
+    <h1 class="title">Current Projects</h1>
+
+  </div>
+  <div class="container">
+
+    @foreach ($projects as $project)
+    <div class="container box is-fluid">
+
+      <ul class="is-dark" style="margin-bottom: 2em">
+        <h4 class="subtitle">Name: <a href="/laravel/projects_app/public/projects/{{$project->id}}">{{$project->title}}</a></h4>
+        <p>Owner: {{$project->owner_of_the_project}}</p>
+      </ul>
+
+    </div>
+    @endforeach
+
+  </div>
+@endsection
