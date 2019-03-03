13e5093e20dbe0df6845faf9e3b6e99ea8d3bdaf
Robert Marczak <robert.m.1305@gmail.com>
2019-03-03 16:25:12 +0000

    Recreated Controller and Model files for tasks.

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -14,7 +14,7 @@ class TasksController extends Controller
      */
     public function index()
     {
-        //
+        dd('index');
     }
 
     /**
@@ -24,7 +24,7 @@ class TasksController extends Controller
      */
     public function create()
     {
-        //
+        dd('create');
     }
 
     /**
@@ -35,7 +35,7 @@ class TasksController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd('store');
     }
 
     /**
@@ -46,7 +46,7 @@ class TasksController extends Controller
      */
     public function show(Task $task)
     {
-        //
+        dd('show');
     }
 
     /**
@@ -57,7 +57,7 @@ class TasksController extends Controller
      */
     public function edit(Task $task)
     {
-        //
+        dd('edit');
     }
 
     /**
@@ -69,7 +69,7 @@ class TasksController extends Controller
      */
     public function update(Request $request, Task $task)
     {
-        //
+        dd('update');
     }
 
     /**
@@ -80,6 +80,6 @@ class TasksController extends Controller
      */
     public function destroy(Task $task)
     {
-        //
+        dd('destroy');
     }
 }
