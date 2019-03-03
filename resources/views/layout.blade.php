3764c28c42ba41006e75f32a084a9a2015e51014
Robert Marczak <robert.m.1305@gmail.com>
2019-03-03 19:45:59 +0000

    Added link to bulma framework.

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
         <title>@yield('page_title', 'Projects App')</title>
 
     </head>
