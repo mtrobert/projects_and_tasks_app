5bfc7037fff4ae4bd7d1d40f2538d7cbe007acaf
Robert Marczak <robert.m.1305@gmail.com>
2019-03-11 10:49:35 +0000

    Put all project data in one container and added edit and delete buttons.

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -18,31 +18,36 @@
           <p class="is-italic">{{$project->title}}</p>
         </div>
 
-    </div>
-
-    <div class="container box is-fluid">
 
         <div class="is-dark" style="margin-bottom: 2em;">
           <h4 class="subtitle has-text-black-bis has-text-weight-semibold">Owner</h4>
           <p class="is-italic">{{$project->owner}}</p>
         </div>
 
-    </div>
-
-    <div class="container box is-fluid">
 
         <div class="is-dark" style="margin-bottom: 2em;">
           <h4 class="subtitle has-text-black-bis has-text-weight-semibold">Description</h4>
           <p class="is-italic">{{$project->description}}</p>
         </div>
 
-    </div>
-
-    <div class="container box is-fluid">
 
         <div class="is-dark" style="margin-bottom: 2em;">
           <h4 class="subtitle has-text-black-bis has-text-weight-semibold">Created</h4>
           <p class="is-italic">{{$project->created_at}}</p>
+        </div>
+
+        <div class="level">
+          <button class="button level-left"><a href="{{env('base_url')}}projects/{{ $project->id }}/edit">Edit</a></button>
+
+          <form class="level-right" action="{{ env('base_url') }}projects/{{$project->id}}" method="post">
+            @csrf
+            {{ method_field('DELETE') }}
+
+          <div class="control">
+            <button type="submit" class="button has-text-danger">Delete Project</button>
+          </div>
+
+          </form>
         </div>
 
     </div>
