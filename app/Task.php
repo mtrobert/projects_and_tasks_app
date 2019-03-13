a39afb48551777f5cc00273e83e49bf10435e90f
Robert Marczak <robert.m.1305@gmail.com>
2019-03-13 12:16:33 +0000

    Added project_id and description to the fillable property.

--- app/Task.php
+++ app/Task.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    protected $fillable = ['completed'];
+    protected $fillable = ['completed', 'project_id', 'description'];
 }
