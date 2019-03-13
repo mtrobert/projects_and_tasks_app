4dd73df2ff81d985a6fec6347e99e94a6c215113
Robert Marczak <robert.m.1305@gmail.com>
2019-03-13 09:36:39 +0000

    Added text decoration for is-complete class within the style tags

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,11 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.4/css/bulma.min.css">
         <title>@yield('page_title', 'Projects App')</title>
-
+        <style media="screen">
+          .is-complete{
+            text-decoration: line-through;
+          }
+        </style>
     </head>
     <body>
         @include('nav')
