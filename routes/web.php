6fdd03c2720740f0c8f76344d2705a48100ed616
Robert Marczak <robert.m.1305@gmail.com>
2019-03-13 10:27:36 +0000

    Registered a route for deleting the project.

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@
 Route::get('/', 'ProjectsController@index');
 Route::get('/projects/create', 'ProjectsController@create');
 Route::post('/projects', 'ProjectsController@store');
+Route::delete('/projects/{project}', 'ProjectsController@destroy');
 Route::get('/projects/{project}', 'ProjectsController@show');
 Route::get('projects/{project}/edit', 'ProjectsController@edit');
 Route::patch('/tasks/{task}', 'ProjectTasksController@update');
